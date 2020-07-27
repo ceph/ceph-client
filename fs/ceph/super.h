9559eb5977bbdbdadbf6c33491ac9d3e25d2f188
Jeff Layton <jlayton@kernel.org>
2020-07-27 10:16:09 -0400

    ceph: fscrypt_auth handling for ceph
    Most fscrypt-enabled filesystems store the crypto context in an xattr,
    but that's problematic for ceph as xatts are governed by the XATTR cap,
    but we really want the crypto context as part of the AUTH cap.
    
    Because of this, the MDS has added two new inode metadata fields:
    fscrypt_auth and fscrypt_file. The former is used to hold the crypto
    context, and the latter is used to track the real file size.
    
    Parse new fscrypt_auth and fscrypt_file fields in inode traces. For now,
    we don't use fscrypt_file, but fscrypt_auth is used to hold the fscrypt
    context.
    
    Allow the client to use a setattr request for setting the fscrypt_auth
    field. Since this is not a standard setattr request from the VFS, we add
    a new field to __ceph_setattr that carries ceph-specific inode attrs.
    
    Have the set_context op do a setattr that sets the fscrypt_auth value,
    and get_context just return the contents of that field (since it should
    always be available).
    
    Reviewed-by: Xiubo Li <xiubli@redhat.com>
    Signed-off-by: Jeff Layton <jlayton@kernel.org>

--- fs/ceph/super.h
+++ fs/ceph/super.h
@@ -443,6 +443,13 @@ struct ceph_inode_info {
 
 	struct work_struct i_work;
 	unsigned long  i_work_mask;
+
+#ifdef CONFIG_FS_ENCRYPTION
+	u32 fscrypt_auth_len;
+	u32 fscrypt_file_len;
+	u8 *fscrypt_auth;
+	u8 *fscrypt_file;
+#endif
 };
 
 static inline struct ceph_inode_info *
@@ -1048,7 +1055,12 @@ static inline int ceph_do_getattr(struct inode *inode, int mask, bool force)
 }
 extern int ceph_permission(struct user_namespace *mnt_userns,
 			   struct inode *inode, int mask);
-extern int __ceph_setattr(struct inode *inode, struct iattr *attr);
+
+struct ceph_iattr {
+	struct ceph_fscrypt_auth	*fscrypt_auth;
+};
+
+extern int __ceph_setattr(struct inode *inode, struct iattr *attr, struct ceph_iattr *cia);
 extern int ceph_setattr(struct user_namespace *mnt_userns,
 			struct dentry *dentry, struct iattr *attr);
 extern int ceph_getattr(struct user_namespace *mnt_userns,
