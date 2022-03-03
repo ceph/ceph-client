ea1ca9f791a11ac93ee3224250b0720d62237b84
David Howells <dhowells@redhat.com>
2022-03-03 16:23:48 +0000

    afs: Enable DIO read through netfslib
    Enable DIO reads, passing them off to netfslib to perform.
    
    Note that this causes xfstests to malfunction as it can't cope with DIO
    reads being possible, but not DIO writes.
    
    Signed-off-by: David Howells <dhowells@redhat.com>
    cc: linux-afs@lists.infradead.org
    cc: linux-cachefs@redhat.com

--- fs/afs/file.c
+++ fs/afs/file.c
@@ -47,6 +47,7 @@ const struct inode_operations afs_file_inode_operations = {
 };
 
 const struct address_space_operations afs_file_aops = {
+	.direct_IO	= noop_direct_IO,
 	.readpage	= netfs_readpage,
 	.readahead	= netfs_readahead,
 	.dirty_folio	= afs_dirty_folio,
@@ -479,6 +480,9 @@ static ssize_t afs_file_read_iter(struct kiocb *iocb, struct iov_iter *iter)
 	ret = afs_validate(vnode, af->key);
 	if (ret < 0)
 		return ret;
+
+	if (iocb->ki_flags & IOCB_DIRECT)
+		return netfs_direct_read_iter(iocb, iter);
 
 	return generic_file_read_iter(iocb, iter);
 }
