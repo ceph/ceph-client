20effa34c72a81b37476d7d1000c7bc3983dd460
Xiubo Li <xiubli@redhat.com>
2022-08-18 12:56:05 +0800

    [DO NOT MERGE] ceph: BUG if MDS changed truncate_seq with client caps still outstanding
    We need to trigger to crash the kernel and fail the qa tests to
    get more infomation about the bug.
    
    URL: https://tracker.ceph.com/issues/56693
    Signed-off-by: Xiubo Li <xiubli@redhat.com>

--- fs/ceph/caps.c
+++ fs/ceph/caps.c
@@ -3547,7 +3547,7 @@ static void handle_cap_grant(struct inode *inode,
 		queue_trunc = ceph_fill_file_size(inode, extra_info->issued,
 					le32_to_cpu(grant->truncate_seq),
 					le64_to_cpu(grant->truncate_size),
-					size);
+					size, newcaps);
 	}
 
 	if (ci->i_auth_cap == cap && (newcaps & CEPH_CAP_ANY_FILE_WR)) {
@@ -3917,7 +3917,8 @@ static bool handle_cap_trunc(struct inode *inode,
 	dout("handle_cap_trunc inode %p mds%d seq %d to %lld seq %d\n",
 	     inode, mds, seq, truncate_size, truncate_seq);
 	queue_trunc = ceph_fill_file_size(inode, issued,
-					  truncate_seq, truncate_size, size);
+					  truncate_seq, truncate_size,
+					  size, 0);
 	return queue_trunc;
 }
 
