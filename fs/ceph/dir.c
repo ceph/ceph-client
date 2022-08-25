e2a5c43c886672800cf9b2c56c36ff2cfcffe57b
Luís Henriques <lhenriques@suse.de>
2022-08-25 09:31:31 -0400

    ceph: prevent snapshots to be created in encrypted locked directories
    With snapshot names encryption we can not allow snapshots to be created in
    locked directories because the names wouldn't be encrypted.  This patch
    forces the directory to be unlocked to allow a snapshot to be created.
    
    Signed-off-by: Luís Henriques <lhenriques@suse.de>
    Reviewed-by: Xiubo Li <xiubli@redhat.com>
    Signed-off-by: Jeff Layton <jlayton@kernel.org>

--- fs/ceph/dir.c
+++ fs/ceph/dir.c
@@ -1084,6 +1084,11 @@ static int ceph_mkdir(struct user_namespace *mnt_userns, struct inode *dir,
 		err = -EDQUOT;
 		goto out;
 	}
+	if ((op == CEPH_MDS_OP_MKSNAP) && IS_ENCRYPTED(dir) &&
+	    !fscrypt_has_encryption_key(dir)) {
+		err = -ENOKEY;
+		goto out;
+	}
 
 
 	req = ceph_mdsc_create_request(mdsc, op, USE_AUTH_MDS);
