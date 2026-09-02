799166d9a4f83fa47e3b656835da882bd00567b2
Tal Zussman <tz2294@columbia.edu>
2026-09-02 12:11:34 -0400

    ceph: rename ceph_check_page_before_write() to ceph_check_folio_before_write()
    This function only operates on folios, so rename it accordingly.
    
    Signed-off-by: Tal Zussman <tz2294@columbia.edu>

--- fs/ceph/addr.c
+++ fs/ceph/addr.c
@@ -1204,10 +1204,10 @@ bool can_next_page_be_processed(struct ceph_writeback_ctl *ceph_wbc,
 }
 
 static
-int ceph_check_page_before_write(struct address_space *mapping,
-				 struct writeback_control *wbc,
-				 struct ceph_writeback_ctl *ceph_wbc,
-				 struct folio *folio)
+int ceph_check_folio_before_write(struct address_space *mapping,
+				  struct writeback_control *wbc,
+				  struct ceph_writeback_ctl *ceph_wbc,
+				  struct folio *folio)
 {
 	struct inode *inode = mapping->host;
 	struct ceph_fs_client *fsc = ceph_inode_to_fs_client(inode);
@@ -1391,8 +1391,8 @@ void ceph_process_folio_batch(struct address_space *mapping,
 		else if (!folio_trylock(folio))
 			break;
 
-		rc = ceph_check_page_before_write(mapping, wbc,
-						  ceph_wbc, folio);
+		rc = ceph_check_folio_before_write(mapping, wbc,
+						   ceph_wbc, folio);
 		if (rc == -ENODATA) {
 			folio_unlock(folio);
 			folio_put(folio);
